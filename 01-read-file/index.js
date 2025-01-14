874a1cb535e10258c59c6d3f586e90e971fb9751
stenyukovs <s.stenyukov@mixvel.com>
2025-01-14 20:27:44 +0300

    feat: 01,02 done

---
+++ 01-read-file/index.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.createReadStream(path.join(__dirname, 'text.txt')).pipe(process.stdout);
---
+++ 02-write-file/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+const writeStream = fs.createWriteStream(path.join(__dirname, 'output.txt'), {
+  flags: 'a',
+});
+
+console.log(
+  'Enter text to write to the file. Type "exit" or press Ctrl+C to quit.',
+);
+
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+
+  if (input.toLowerCase() === 'exit') exitProgram();
+  else {
+    writeStream.write(input + '\n', (err) => {
+      if (err) console.error('Unexpected error when writing:', err);
+    });
+  }
+});
+
+process.on('SIGINT', exitProgram);
+
+function exitProgram() {
+  console.log('Thank you for using the program. Goodbye!');
+  writeStream.end();
+  process.exit();
+}
